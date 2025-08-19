7f13dd819224c2686e0b61ebafde0b50c422c02a
preethipantangi <pantangisaipreethi@gmail.com>
2025-08-18 22:52:19 -0400

    Updated experience

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -91,28 +91,40 @@ const about = {
         role: "Programmer Analyst",
         achievements: [
           <>
-            Led the migration of a monolithic JSP-based crash reporting system to a React JS and Spring Boot microservices architecture, 
-            reducing page load time by 40%. Rearchitected the frontend with component-based approach, lazy loading, and API-level 
-            caching, resulting in highly responsive UI, significantly enhancing user experience and operational efficiency. 
+            Driving the migration of NJ Crash from JSP/JSF to Angular + Spring Boot; owning requirements gathering, data mapping, design documentation, and delivering end-to-end development of UI modules, APIs, and database integrations.
           </>,
           <>
-            Designed the database schema, integrated backend services with external systems such as Purvis and Flock via REST APIs 
-            and implemented real-time schedulers using Java single-threaded and Lambda, reducing data sync latency by 60%. 
-            Documented query logic, database schema updates, and data flow diagrams in Confluence to support cross-team. 
+            Modernized MA crash reporting platform by migrating from JSP monolith into a React + Spring Boot microservices architecture, introduced API-level caching, lazy loading. Achieved 40% lower page load times, with adoption by 6 agencies.
           </>,
           <>
-            Authored and optimized complex SQL queries, stored procedures, in Informix and Oracle, improving XML report generation 
-            speeds by 25% and supporting normalized data flow, maintaining referential integrity, and improving query performance. 
+            Architected and deployed Purvis CAD integration using REST APIs, SOAP, and asynchronous schedulers, to transmit data to auto-open fire station gates within 5 seconds of incident creation; achieved 99.9% uptime across 5+ fire stations, coordinated live testing with Somerville Fire Department, and mentored and conducted code review of offshore team of 5 
           </>,
           <>
-            Programmed a Java-based Data Import/Export and Table Upgrade Automation Tool using Spring Boot, React JS, and 
-            Informix, enabling XML generation with preserved key relationships and automated schema migrations across versions, 
-            reducing manual migration efforts by 40 hours per release and cut schema upgrade time from 3 days to under 4 hours. 
+            Built a Spring Boot + React automation framework for data import/export and schema upgrades tool, reducing manual migration effort by 40 hours per release and cutting DB upgrade time from 4 hours to less than 20 minutes.
           </>,
           <>
-            Managed and mentored an offshore development team of 5+ engineers, conducted 30+ code reviews per release cycle, 
-            delegated tasks using Agile methodologies, and maintained 95% on-time delivery across 2 high-impact state agency 
-            projects, improving development efficiency by 20%.
+            Engineered a real-time Flock API integration to transmit incident data directly to police units across 25+ agencies; reduced manual radio communication by 60%, enabling faster on-ground response and improved coordination during emergencies. 
+          </>,
+          <>
+            Optimized SQL queries and stored procedures (Informix & Oracle), boosting XML report generation throughput by 25% and ensuring consistent normalized data flow, maintaining referential integrity, and across distributed systems.
+          </>,
+          <>
+            Implemented CI/CD pipelines with Jenkins and Docker to automate builds, testing, and deployments of React , Spring Boot services, reducing deployment times and enabling zero-downtime releases across staging and production environments.
+          </>
+        ],
+        images: [],
+      },
+      {
+        company: "George Mason University",
+        timeframe: "May 2023 - May 2024",
+        role: "Software Engineer Specialist",
+        achievements: [
+          <>
+            Built an AI-powered PDF Q&A tool using ReactJS, Python, FastAPI, and OpenAI API, enabling file uploads and document-based Q&A; integrated with U.S. Web Design System for accessibility compliance. 
+
+          </>,
+          <>
+            Enhanced compositional generalization in semantic parsing by reproducing and optimizing the TPOL framework with BERT, MBART, and RoBERTa, improving multilingual performance on GEO-Aligned datasets and contributing to open source.
           </>
         ],
         images: [],
@@ -123,26 +135,18 @@ const about = {
         role: "Software Engineer Specialist",
         achievements: [
           <>
-             Architected the Schedule Tab feature, by collaborating with cross-functional teams and leveraging asynchronous 
-            programming in JavaScript, and processing of REST API requests using Spring Boot and GraphQL reducing latency to ~4 RPS. 
+            Engineered a Schedule Tab feature for PLM applications, improving response times by 80% via GraphQL batch requests, async programming, and server-side pagination; designed an Adapter-pattern-based integration layer to bridge the Portfolio and Project Plan applications, ensuring smooth and scalable data flow. 
           </>,
           <>
-            Utilized Redux for state management and optimized UI performance by integrating server-side pagination and reusable 
-            functional components, resulting in a 56% better page load time and smoother experience in PLM modules. 
+            Designed and developed a modular Roadmap component for project planning, later adopted as a shared standard across the org; accelerated the feature rollout by 30%, and reduced cross-team integration effort by 40 hours.
           </>,
           <>
-            Adopted Test Driven Development(TDD) with Jest, using mocking and stubbing techniques for comprehensive test coverage 
-            in an incremental development process, leading to 95% code coverage and significantly reducing post-release bugs.
+            Improved UI performance and scalability by implementing Redux state management and reusable functional components, delivering a 56% improvement in load times.
+
           </>,
           <>
-            Engineered a reusable, modular roadmap component for PLM applications using JavaScript and RESTful APIs, enabling 
-            seamless integration across multiple platforms and reducing feature rollout time by 30%, while improving code reusability 
-            and maintainability.
-          </>, 
-          <>
-            Utilized SVN version control and CI/CD pipelines to ensure consistent source code integration with Agile setup, improving 
-            code quality through code reviews for junior developers, resulting in reduction of critical bugs by 15%.
-          </>,
+            Advocated for Test-Driven Development with Jest, achieving 95% code coverage and reducing post-release defects by 15%, supported CI/CD pipelines and peer code reviews, ensuring maintainability, and reduction of critical bugs.
+          </>
         ],
         images: [],
       },
@@ -152,29 +156,19 @@ const about = {
         role: "Software Engineer",
         achievements: [
           <>
-            Devised and developed RESTful microservices using Spring Boot and JAX-RS, adhering to microservices architecture best 
-            practices and supporting scalable backend services across multiple modules and deployed in docker containers. 
+            Led the refactor of 100K+ lines of legacy Java code to migrate Disney’s Android apps to the latest OS version, enabling multilingual support, optimizing memory usage, and boosting engagement across millions of daily users.
           </>,
           <>
-            Adopted Spring Cloud Config Server for centralized configuration management and integrated Splunk for real-time, 
-            centralized logging and monitoring, enhancing observability across services.
+            Developed a medicine recommendation platform for Bayer using Angular, Java, and Python—scraped 1,000+ medicine images using Beautiful Soup and later extended it to React Native for seamless iOS/Android access.
           </>,
           <>
-            Built responsive frontend modules using Angular and Spring Boot, leveraging NPM, Gulp, and Flux for modular view 
-            rendering and efficient routing, improving load performance and developer productivity.
+            Built responsive frontend modules with Angular and Spring Boot, applying modular view rendering and optimized routing strategies; achieved 25% faster load times and significantly improved developer productivity through streamlined builds (NPM, Gulp).
           </>,
           <>
-            Engineered a medicine recommendation platform with Angular and web scraping techniques, enriching content with 
-            1,000+ medicine images, extending the UI to React Native, enabling seamless cross-platform access on iOS and Android. 
+            Improved system reliability by 30% with a Java-based Scheduled Task Monitor and AWS CloudWatch integration for real-time logging/alerts.
           </>,
           <>
-            Refactored 100,000+ lines of legacy Java code for Disney’s Android gaming app, applying code modularization, memory 
-            management improvements, and lazy loading techniques to boost app performance, enabling multilingual support, and 
-            significantly increase user engagement and retention. 
-          </>,
-          <>
-            Resolved high-priority production issues through detailed root cause analysis and built a Java-based Scheduled Task 
-            Monitor using Windows Task Scheduler, resulting in a 30% improvement in system reliability and uptime. 
+            Designed and deployed RESTful microservices with Spring Boot and JAX-RS, containerized with Docker, and centralized config with Spring Cloud Config Server and Splunk, ensuring scalable deployments across environments.
           </>
         ],
         images: [],
